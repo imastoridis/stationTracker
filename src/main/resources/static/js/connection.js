e17980ca004162c1482465262e4c82d08cb5136e
imasto <imastoridis@gmail.com>
2026-02-17 13:54:40 +0100

    Update connection

--- src/main/resources/static/js/connection.js
+++ src/main/resources/static/js/connection.js
@@ -1,3 +1,11 @@
+// Define your base URLs
+function getUrl(isProduction){
+    const API_BASE_URL = isProduction
+        ? '/stationTracker/'
+        : '/';
+    return API_BASE_URL
+}
+
 /* Connection to api */
 let sseConnection = null;
 
@@ -5,9 +13,11 @@ let sseConnection = null;
 function connectToStream() {
 
     if (sseConnection) sseConnection.close();
+    // Determine environment based on the browser's current URL
+    var isProduction = window.location.hostname === 'imastoridis.com';
+    const API_BASE_URL = getUrl(isProduction)
 
-    sseConnection = new EventSource('/api/stream');
-
+    sseConnection = new EventSource(API_BASE_URL + 'api/stream');
     // Standard message listener for updates
     sseConnection.onmessage = (event) => {
         let status = JSON.parse(event.data);
